cdc31ff7c70d10c7ad8f57867a03fbf357239595
makerseok <maker7788@gmail.com>
2022-05-19 09:21:51 +0900

    BodyParser로 회원가입 기능 구현

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+//application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://makerseok:as145792@boilerplate-shard-00-00.itfrr.mongodb.net:27017,boilerplate-shard-00-01.itfrr.mongodb.net:27017,boilerplate-shard-00-02.itfrr.mongodb.net:27017/?ssl=true&replicaSet=atlas-dkiu5s-shard-0&authSource=admin&retryWrites=true&w=majority')
@@ -9,6 +16,16 @@ mongoose.connect('mongodb://makerseok:as145792@boilerplate-shard-00-00.itfrr.mon
 
 app.get('/', (req, res) => {
     res.send('Hello World!~!@!@')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
