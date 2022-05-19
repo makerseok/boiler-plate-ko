1411b563445746ce3bec66d2974a97449a41835f
makerseok <maker7788@gmail.com>
2022-05-19 09:30:14 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+
 const { User } = require("./models/User")
+
+const config = require('./config/key')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -10,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://makerseok:as145792@boilerplate-shard-00-00.itfrr.mongodb.net:27017,boilerplate-shard-00-01.itfrr.mongodb.net:27017,boilerplate-shard-00-02.itfrr.mongodb.net:27017/?ssl=true&replicaSet=atlas-dkiu5s-shard-0&authSource=admin&retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
     .then(() => console.log('MongoDB Connected..'))
     .catch(err => console.log(err))
 
