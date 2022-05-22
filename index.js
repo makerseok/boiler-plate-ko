b7afff964d59e2fdcd9b8b68169b80d1d220e350
makerseok <maker7788@gmail.com>
2022-05-22 17:27:36 +0900

    로그아웃 기능 구현

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!~!@!@')
 })
 
-app.post('api/users/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
     user.save((err, userInfo) => {
         if (err) return res.json({ success: false, err })
@@ -34,7 +34,7 @@ app.post('api/users/register', (req, res) => {
     })
 })
 
-app.post('api/users/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
             return res.json({
@@ -68,6 +68,17 @@ app.get('/api/users/auth', auth, (req, res) => {
         role: req.user.role,
         image: req.user.image
     })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" }
+        , (err, user) => {
+            if (err) return res.json({ success: false, err })
+            return res.status(200).send({
+                success: true
+            })
+        })
 })
 
 app.listen(port, () => {
