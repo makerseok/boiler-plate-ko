4aa2dc2044eb7768a227202a3b57141bf4e1c2dc
makerseok <maker7788@gmail.com>
2022-05-24 20:42:21 +0900

    로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,25 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
-function LandingPage() {
+import { useNavigate } from 'react-router-dom';
+
+function LandingPage(props) {
+    const navigate = useNavigate();
+
     useEffect(() => {
         axios.get('/api/hello')
             .then(response => console.log(response))
     }, [])
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+            .then(response => {
+                if (response.data.success) {
+                    navigate('/login')
+                } else {
+                    alert('로그아웃 하는데 실패했습니다.')
+                }
+            })
+    }
 
     return (
         <div style={{
@@ -12,6 +27,10 @@ function LandingPage() {
             , width: '100%', height: '100vh'
         }}>
             시작 페이지
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
